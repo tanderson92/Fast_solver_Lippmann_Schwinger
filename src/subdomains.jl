ce28968f98f2a138f58c3e5132d250ec0769b546
lzepeda <lzepeda@math.mit.edu>
2015-11-30 11:11:49 -0800

    fixed some bugs with subdomains now test ddm 3 subdomains works.

--- src/subdomains.jl
+++ src/subdomains.jl
@@ -20,7 +20,7 @@ type Subdomain
     function Subdomain(As,AG,Msp, x,y, ind1, indn, ndelta, h, nu,k)
         println("Building a subdomain")
         indStart   = ind1==1? 1 : ind1-ndelta;
-        indFinish = indn==length(y) ? length(y) : ind1+ndelta;
+        indFinish = indn==length(y) ? length(y) : indn+ndelta;
         y1 = y[indStart : indFinish]
         x1 = copy(x)
         n1 = length(x1)
@@ -33,18 +33,28 @@ type Subdomain
         ylim = [y[ind1] y[indn]]
         #the domain is decomposed in two subdomains
         println("Defining the filter")
-        fcut(y) = e.^(-1./y).*(y.>0)
-        fcut(y,alpha) = fcut(y)./(fcut(y) + fcut(alpha - y))
-        midy = (ylim[2]+ ylim[1])/2
-        widthy = abs(ylim[2] - ylim[1])/2
-        width = h*(ndelta-1);
-        filter(x,y) =  0*x + (y.<=(ylim[2]+h)).*(y.>=(ylim[1]-h)).*(1- fcut(abs(y - width) - witdhy-h, width))  ; #add the rest of the filter in here
-        nu1(x,y) = filter1(x,y).*nu(x,y);
+        # fcut(y) = e.^(-1./y).*(y.>0)
+        # fcut(y,alpha) = fcut(y)./(fcut(y) + fcut(alpha - y))
+        # midy = (ylim[2]+ ylim[1])/2
+        # widthy = abs(ylim[2] - ylim[1])/2
+        # width = h*(ndelta-1);
+        # filter(x,y) =  0*x + (y.<=(ylim[2]+h)).*(y.>=(ylim[1]-h)).*(1- fcut(abs(y - width) - witdhy-h, width))  ; #add the rest of the filter in here
+        spline(y) = (y.<0) +  (y.>=0).*(y.<1).*(2*y.^3 - 3*y.^2 + 1)
+        filter(a1,b1,b2,a2,y) = (y.>=a1).*( (y.<b1).*spline(1/(abs(b1-a1)).*(-y+b1)) + (y.>=b1).*(y.<b2) + (y.>=b2).*spline(1/(abs(b2-a2)).*(y-b2)))
+
+        # defining the speed with the correct cut-off
+        if ind1 == 1
+            nu1(x,y) = filter(y1[1]-h, y1[1], y1[end-ndelta+2], y1[end-2] , y).*nu(x,y);
+        elseif indn == length(y)
+             nu1(x,y) = filter(y1[3], y1[ndelta-2], y1[end], y1[end]+h , y).*nu(x,y);
+        else
+            nu1(x,y) = filter(y1[3], y1[ndelta-2], y1[end-ndelta+2], y1[end-2] , y).*nu(x,y);
+        end
 
         println("Getting the Indices")
         indVolInt =  find((Y.>=y[ind1]).*(Y.<= y[indn]))
         indVol    =  find((Y.>=y1[1]).*(Y.<= y1[end]))
-        indVolIntLocal =  find(Y1.< h)
+        indVolIntLocal =  find((Y1.>=y[ind1]).*(Y1.<= y[indn]))
 
         println("Building the local matrices")
         Mapproxsp =  (As + k^2*(AG*spdiagm(nu1(X,Y))))[indVol,indVol];
@@ -53,22 +63,23 @@ type Subdomain
         if ind1 != 1
             println("Adding the Boundary conditions at the top")
             Mapproxsp[1:2*n, 1:2*n] = Msp[1:2*n, 1:2*n];
-        elseif indn != length(y)
+        end
+        if indn != length(y)
             println("Adding the Boundary conditions at the bottom")
             Mapproxsp[end-2*n1+1:end, end-2*n1+1:end]=Msp[end-2*n1+1:end, end-2*n1+1:end];
         end
 
         println("Obtaining the indices for the traces")
         if ind1 != 1
-            ind_0  = find(Y1.== y[ndelta]);
-            ind_1  = find(Y1.== y[ndelta+1]);
+            ind_0  = find(Y1.== y1[ndelta]);
+            ind_1  = find(Y1.== y1[ndelta+1]);
         else
             ind_0  = [];
             ind_1  = [];
         end
         if indn != length(y)
-            ind_n  = find(Y1.== y[m1-ndelta]);
-            ind_np = find(Y1.== y[m1-ndelta+1]);
+            ind_n  = find(Y1.== y1[m1-ndelta]);
+            ind_np = find(Y1.== y1[m1-ndelta+1]);
         else
             ind_n  = [];
             ind_np  = [];
@@ -78,7 +89,7 @@ type Subdomain
         Minv = lufact(Mapproxsp);
 
         new(n1,m1, h, ndelta*h,ind_0, ind_1, ind_n, ind_np, Minv,Mapproxsp ,
-            x, y, [y[ind1] y[indn]], indVolInt, indVol,
+            x, y1, [y[ind1] y[indn]], indVolInt, indVol,
             indVolIntLocal)
     end
 end
@@ -122,11 +133,11 @@ function applyBlockOperator(subdomain::Subdomain,v0,v1,vN,vNp)
     # filling the source with the correct single and double layer potentials
     # TO DO: put the correct operators in here (we need to multiply the traces)
     # by the correct weights
-    f[subdomain.ind_0 ] =  v0;
-    f[subdomain.ind_1 ] = -v1;
-    f[subdomain.ind_n ] = -vN;
-    f[subdomain.ind_np] =  vNp;
-    f = f*(1/subdomain.h)^2;
+    f[subdomain.ind_1 ] =  subdomain.H[subdomain.ind_1,subdomain.ind_0]*v0;
+    f[subdomain.ind_0 ] = -subdomain.H[subdomain.ind_0,subdomain.ind_1]*v1;
+    f[subdomain.ind_np] = -subdomain.H[subdomain.ind_np,subdomain.ind_n]*vN;
+    f[subdomain.ind_n ] =  subdomain.H[subdomain.ind_n,subdomain.ind_np]*vNp;
+
     u = solve(subdomain, f[:]);
 
     u0  = u[subdomain.ind_0  ];
---
+++ tests/test_ddm_3subdomainsTypes.jl
@@ -0,0 +1,190 @@
+# small script to test how to perform the domain decomposition
+# we try to compute fast solution without tampering with A directly,
+
+
+# Clean version of the DDM ode
+
+using PyPlot
+using Devectorize
+
+
+include("../src/FastConvolution.jl")
+include("../src/quadratures.jl")
+include("../src/subdomains.jl")
+#Defining Omega
+h = 0.01
+k = 1/h
+
+# size of box
+a  = 1
+x = -a/2:h:a/2
+y = -a/2:h:a/2
+n = length(x)
+m = length(y)
+N = n*m
+X = repmat(x, 1, m)[:]
+Y = repmat(y', n,1)[:]
+# we solve \triangle u + k^2(1 + nu(x))u = 0
+# in particular we compute the scattering problem
+
+
+# we extract from a "tabulated" dictionary
+# the good modification for the quadrature modification
+(ppw,D) = referenceValsTrapRule();
+D0 = D[1];
+
+#G = buildConvMatrix(k,X,Y,D0,h)
+
+
+#the domain is decomposed in two subdomains
+
+nu(x,y) = -0.3*exp(-20*(x.^2 + y.^2)).*(abs(x).<0.46).*(abs(y).<0.46);
+
+Ge = buildGConv(x,y,h,n,m,D0,k);
+GFFT = fft(Ge);
+
+fastconv = FastM(GFFT,nu(X,Y),3*n-2,3*m-2,n, m, k);
+
+u_inc = exp(k*im*Y);
+
+# bdyInd = setdiff(collect(1:N), volInd);
+
+As = buildSparseA(k,X,Y,D0, n ,m);
+
+AG = buildSparseAG(k,X,Y,D0, n ,m);
+
+# need to check everything here :S
+
+Mapproxsp = k^2*(AG*spdiagm(nu(X,Y)));
+Mapproxsp = As + Mapproxsp;
+
+
+S1 = Subdomain(As,AG,Mapproxsp,x,y, 1, round(Integer, m/3), 30, h, nu, k);
+S2 = Subdomain(As,AG,Mapproxsp,x,y,  round(Integer, m/3)+1, round(Integer, 2*m/3), 30, h, nu, k);
+S3 = Subdomain(As,AG,Mapproxsp,x,y,  round(Integer, 2*m/3)+1, m, 30, h, nu, k);
+
+# compute the right hand side
+rhs = -(fastconv*u_inc - u_inc);
+rhsA = (As*rhs);
+
+# defining the Preconditioner
+function precondIT(source)
+    rhsA1 = source[S1.indVolInt]
+    rhsA2 = source[S2.indVolInt]
+    rhsA3 = source[S3.indVolInt]
+    # put the rhs in the correct dumme vector
+    @assert norm(source - vcat(rhsA1,rhsA2,rhsA3))<eps(1.0)
+
+    rhsLocal1 = zeros(Complex128,S1.n*S1.m)
+    rhsLocal2 = zeros(Complex128,S2.n*S2.m)
+    rhsLocal3 = zeros(Complex128,S3.n*S3.m)
+
+    rhsLocal1[S1.indVolIntLocal] = copy(rhsA1)
+    rhsLocal2[S2.indVolIntLocal] = copy(rhsA2)
+    rhsLocal3[S3.indVolIntLocal] = copy(rhsA3)
+
+    # solve for each one
+
+    u1 = solve(S1,rhsLocal1);
+    u2 = solve(S2,rhsLocal2);
+    u3 = solve(S3,rhsLocal3);
+
+    #sweep up and down (just communicate trhoug the boundary)
+
+    u1_N = u1[S1.ind_n];
+    u1_N1 = u1[S1.ind_np];
+
+    u2_0 = u2[S2.ind_0];
+    u2_1 = u2[S2.ind_1];
+    u2_N =  u2[S2.ind_n];
+    u2_N1 = u2[S2.ind_np];
+
+    u3_0 = u3[S3.ind_0];
+    u3_1 = u3[S3.ind_1];
+
+
+    #rhsLocal1[S1.ind_n]  += -S1.H[S1.ind_n,S1.ind_np]*u2_1
+    #rhsLocal1[S1.ind_np] += S1.H[S1.ind_np,S1.ind_n]*u2_0
+
+    # first sweep
+    rhsLocal2[S2.ind_0] += S2.H[S2.ind_0,S2.ind_1]*u1_N1
+    rhsLocal2[S2.ind_1] += -S2.H[S2.ind_1,S2.ind_0]*u1_N
+
+    u2 = solve(S2,rhsLocal2);
+
+    u22_N  =  u2[S2.ind_n];
+    u22_N1 = u2[S2.ind_np];
+
+    rhsLocal3[S3.ind_0] +=  S3.H[S3.ind_0,S3.ind_1]*u22_N1
+    rhsLocal3[S3.ind_1] += -S3.H[S3.ind_1,S3.ind_0]*u22_N
+
+    u3 = solve(S3,rhsLocal3);
+
+    u3_0 = u3[S3.ind_0];
+    u3_1 = u3[S3.ind_1];
+
+    # backwards sweep
+    # rhsLocal2[S2.indVolIntLocal] = rhsA2
+
+    # rhsLocal2[S2.ind_n]  += -S2.H[S2.ind_n ,S2.ind_np]*u3_1
+    # rhsLocal2[S2.ind_np] +=  S2.H[S2.ind_np,S2.ind_n ]*u3_0
+
+    # u2 = solve(S2,rhsLocal2);
+
+    # u2_0 = u2[S2.ind_0];
+    # u2_1 = u2[S2.ind_1];
+
+    # rhsLocal2[S2.ind_0] += S2.H[S2.ind_0,S2.ind_1]*u1_N1
+    # rhsLocal2[S2.ind_1] += -S2.H[S2.ind_1,S2.ind_0]*u1_N
+
+    # #u2 = solve(S2,rhsLocal2);
+
+    # rhsLocal1[S1.ind_n]  += -S1.H[S1.ind_n,S1.ind_np]*u2_1
+    # rhsLocal1[S1.ind_np] +=  S1.H[S1.ind_np,S1.ind_n]*u2_0
+
+    # u1 = solve(S1,rhsLocal1);
+
+
+    return  vcat(u1[S1.indVolIntLocal],u2[S2.indVolIntLocal],u3[S3.indVolIntLocal]);
+
+end
+
+ApplyPrecond(x) = precondIT(Mapproxsp*x)
+
+using IterativeSolvers
+
+u = zeros(Complex128,N);
+@time info =  gmres!(u, ApplyPrecond, precondIT(As*rhs), restart = 40)
+println("number of iterations for inner solver is ", countnz(info[2].residuals[:]))
+
+# solving the new
+
+
+Minv = lufact(Mapproxsp);
+
+function MinvAp(x)
+    y = zeros(Complex128,N);
+    info =  gmres!(y, ApplyPrecond, precondIT(x), tol = 1e-8, restart = 40)
+    println("number of iterations for inner solver is ", countnz(info[2].residuals[:]))
+    return y
+end
+
+# random vector
+rhs = rand(N) + im*rand(N);
+
+norm(Minv \ rhs - MinvAp(rhs))/norm(rhs)
+
+precond(x) = Minv\(As*(fastconv*x));
+
+# building the RHS from the incident field
+u_inc = exp(k*im*Y);
+rhs = -(fastconv*u_inc - u_inc);
+#x = zeros(Complex128,N);
+#info =  gmres!(x, M, rhs, maxiter  = 60)
+
+
+u = zeros(Complex128,N);
+@time info =  gmres!(u, precond, Minv\(As*rhs))
+println(info[2].residuals[:])
+
+
