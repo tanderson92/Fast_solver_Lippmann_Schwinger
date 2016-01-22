57215fd40e4f20468c24983305afe902b4f16bec
lzepeda <lzepeda@math.mit.edu>
2016-01-21 18:20:44 -0800

    move some parts to another file

--- src/preconditioner.jl
+++ src/preconditioner.jl
@@ -608,5 +608,93 @@ function precondGSOptimized(subDomains, source::Array{Complex128,1})
 end
 
 
+########################################################################################
+#                                                                                      #
+#     Preconditioners for the Integral System                                          #
+#                                                                                      #
+########################################################################################
 
+type IntegralPreconditioner
+    n::Int64  # number of grid points in the x direction
+    nSubs::Int64
+    subDomains
+    nIt::Int64
+    precondtype # preconditioner type Jacobi, GaussSeidel Opt
+    function IntegralPreconditioner(subDomains; nIt = 1, precondtype ="GS")
+        n = subDomains[1].n
+        nSubs = length(subDomains)
+        new(n,nSubs, subDomains,nIt, precondtype) # don't know if it's the best answer
+    end
+end
 
+function \(M::IntegralPreconditioner, b::Array{Complex128,1})
+    #println("Applying the polarized Traces Preconditioner")
+    if M.precondtype == "GS"
+        return PrecondGaussSeidel(M.subArray, b, M.nit)
+    elseif M.precondtype == "Jac"
+        return PrecondJacobi(M.subArray, b, M.nit)
+    end 
+end
+
+function PrecondJacobi(subArray, v, nit)
+    # function to apply the block Jacobi preconditioner using the data
+    # TODO add dimension checks
+    u = 0*v;
+    for ii = 1:nit
+        # splitting the vector in two parts
+        udown = u[1:end/2];
+        uup   = u[(1+end/2):end];
+        # f - Ru^{n-1}
+        if norm(u)!=0
+            udownaux = v[1:end/2]       - applyU(subArray, uup);
+            uupaux   = v[(1+end/2):end] - applyL(subArray, udown);
+        else
+            udownaux = v[1:end/2]
+            uupaux   = v[(1+end/2):end] 
+        end
+
+        vdown = applyDinvDown(subArray,udownaux);
+        vup   = applyDinvUp(subArray,uupaux);
+        u    = vcat(vdown, vup );
+    end
+    return u;
+end
+
+function PrecondGaussSeidel(subArray, v, nit; verbose=false)
+    # function to apply the block Gauss-Seidel Preconditioner
+    # input :   subArray  array pointer to the set of subdomains
+    #           v         rhs to be solved
+    #           nit       number of iterations
+    # output:   u         Approximated solution
+    # using a first guess equals to zero
+
+    println("Applying the preconditioner");
+    u = 0*v;
+    for ii = 1:nit
+
+        # splitting the vector in two parts
+        udown = u[1:end/2];
+        uup   = u[(1+end/2):end];
+
+        # f - Ru^{n-1}
+        if norm(u)  != 0
+            udownaux = v[1:end/2]       - applyU(subArray, uup);
+        else
+            udownaux = v[1:end/2] 
+        end
+        uupaux   = v[(1+end/2):end] ;
+
+        # applying the inverses
+        vdown = applyDinvDown(subArray,udownaux);
+        vup   = applyDinvUp(subArray, uupaux - applyL(subArray,vdown));
+
+        if verbose
+            println("magnitude of the update = ", norm(u[:] -  vcat(vdown, vup ))/norm(v[:]));
+        end
+
+        # concatenatinc the solution
+        u    = vcat(vdown, vup );
+
+    end
+    return u;
+end
