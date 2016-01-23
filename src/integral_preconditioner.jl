9a8e19d5d508f3c2c32afe07c4ee5df6d1baa9fa
lzepeda <lzepeda@math.mit.edu>
2016-01-22 19:36:03 -0800

    introduced some typed optimization on the preconditioner

--- src/integral_preconditioner.jl
+++ src/integral_preconditioner.jl
@@ -10,38 +10,40 @@ type IntegralPreconditioner
     nSubs::Int64
     subDomains
     nIt::Int64
+    P # permutation matrix
     precondtype # preconditioner type Jacobi, GaussSeidel Opt
     function IntegralPreconditioner(subDomains; nIt = 1, precondtype ="GS")
         n = subDomains[1].n
         nSubs = length(subDomains)
-        new(n,nSubs, subDomains,nIt, precondtype) # don't know if it's the best answer
+        P = generatePermutationMatrix(n,nSubs);
+        new(n,nSubs, subDomains,nIt, P, precondtype) # don't know if it's the best answer
     end
 end
 
 function \(M::IntegralPreconditioner, b::Array{Complex128,1})
     #println("Applying the polarized Traces Preconditioner")
     if M.precondtype == "GS"
-        return PrecondGaussSeidel(M.subDomains, b, M.nIt)
+        return PrecondGaussSeidel(M.subDomains, M.P*b, M.nIt)
     elseif M.precondtype == "Jac"
-        return PrecondJacobi(M.subDomains, b, M.nIt)
-    end 
+        return PrecondJacobi(M.subDomains, M.P*b, M.nIt)
+    end
 end
 
-function PrecondJacobi(subArray, v, nit)
+function PrecondJacobi(subArray, v::Array{Complex128,1}, nit)
     # function to apply the block Jacobi preconditioner using the data
     # TODO add dimension checks
     u = 0*v;
     for ii = 1:nit
         # splitting the vector in two parts
-        udown = u[1:end/2];
-        uup   = u[(1+end/2):end];
+        udown = u[1:round(Integer,end/2)];
+        uup   = u[(1+round(Integer,end/2)):end];
         # f - Ru^{n-1}
         if norm(u)!=0
-            udownaux = v[1:end/2]       - applyU(subArray, uup);
-            uupaux   = v[(1+end/2):end] - applyL(subArray, udown);
+            udownaux = v[1:round(Integer,end/2)]       - applyU(subArray, uup);
+            uupaux   = v[(1+round(Integer,end/2)):end] - applyL(subArray, udown);
         else
-            udownaux = v[1:end/2]
-            uupaux   = v[(1+end/2):end] 
+            udownaux = v[1:round(Integer,end/2)]
+            uupaux   = v[(1+round(Integer,end/2)):end]
         end
 
         vdown = applyDinvDown(subArray,udownaux);
@@ -51,7 +53,7 @@ function PrecondJacobi(subArray, v, nit)
     return u;
 end
 
-function PrecondGaussSeidel(subArray, v, nit; verbose=false)
+function PrecondGaussSeidel(subArray, v::Array{Complex128,1}, nit; verbose=false)
     # function to apply the block Gauss-Seidel Preconditioner
     # input :   subArray  array pointer to the set of subdomains
     #           v         rhs to be solved
@@ -64,16 +66,16 @@ function PrecondGaussSeidel(subArray, v, nit; verbose=false)
     for ii = 1:nit
 
         # splitting the vector in two parts
-        udown = u[1:end/2];
-        uup   = u[(1+end/2):end];
+        udown = u[1:round(Integer,end/2)];
+        uup   = u[(1+round(Integer,end/2)):end];
 
         # f - Ru^{n-1}
         if norm(u)  != 0
-            udownaux = v[1:end/2]       - applyU(subArray, uup);
+            udownaux = v[1:round(Integer,end/2)] - applyU(subArray, uup);
         else
-            udownaux = v[1:end/2] 
+            udownaux = v[1:round(Integer,end/2)]
         end
-        uupaux   = v[(1+end/2):end] ;
+        uupaux   = v[(1+round(Integer,end/2)):end] ;
 
         # applying the inverses
         vdown = applyDinvDown(subArray,udownaux);
